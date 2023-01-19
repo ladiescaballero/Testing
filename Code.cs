153d249a80ed0f1ee932b22216e402a13c37a5e2
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-01-18 23:02:25 -0300

    pr

--- Code.cs
+++ Code.cs
@@ -1,7 +1,20 @@
 static void Main(string[]args)
 {
 
-   
+// 📙 221
+// El Arralist
+// Ejemplo de un arrayList
+
+ArrayList datos = new ArrayList();
+ArrayList datosDos = new ArrayList(15);
+
+datos.Add(7);
+datos.Add(n);
+palabras.Add("Hola Mundo");
+
+
+
+
     
 }
 
