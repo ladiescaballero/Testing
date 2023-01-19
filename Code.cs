04760ec7b3cc1ecf503b20b917aa6a7432bec832
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-01-19 14:04:58 -0300

    19/1/2023 -- 14:04

--- Code.cs
+++ Code.cs
@@ -1,20 +1,53 @@
 static void Main(string[]args)
 {
 
-// 📙 221
-// El Arralist
-// Ejemplo de un arrayList
+// 📙 223
 
-ArrayList datos = new ArrayList();
-ArrayList datosDos = new ArrayList(15);
-
-datos.Add(7);
-datos.Add(n);
-palabras.Add("Hola Mundo");
+    foreach(int valor in costo)
+    {
+        Console.WriteLine("el valor es {0} ",costo);
+    }
 
 
+// como acceder a la informacion de un arreglo
+
+// 📙 224
+Console.WriteLine("El dato es {0} ",datos[2]);
+
+impuesto = datos[2] * 0.15f;
+
+datos[2] = 5;
+
+Como obtener la cantidad de elementos de un arreglo
+
+elementos = datos.Count;
+// si sale valor 5 .. los indices son del 0 al 4
 
 
-    
+// Insertar Elementos , Tambien se pueden insertar elementos aparte de Add .
+// el metodo Insert() necesita de dos parametros. el primero es el indice , segundo indice el elemento
+// Es como un actualizar..
+
+datos.Insert(2,5); // valor del indice 2 y el elemento es el 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
 
