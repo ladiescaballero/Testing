e8853bc3287a708d3abd07ae3f4b45d4abd52890
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-02-21 22:52:52 -0300

    22:52

---
+++ SQl Libro 🧮/Sql.sql
@@ -0,0 +1,30 @@
+🧨 <Paginas 28>
+
+Capítulo 1 Introducción a la 
+administración de base 
+de datos
+
+-- Quizás usted no se haya dado cuenta, pero su vida se ve afectada de forma dramática por la tec- 
+-- nología de base de datos. Las bases de datos computarizadas son vitales para el funcionamiento 
+-- de las organizaciones modernas. Usted está en contacto con las bases de datos diariamente a tra- 
+-- vés de actividades como comprar en el supermercado, retirar efectivo de un cajero automático, 
+-- ordenar un libro en línea y registrarse en una clase. Las comodidades de su vida diaria, en parte, 
+-- se deben a la proliferación de las bases de datos computarizadas y a su tecnología.
+-- La tecnología de bases de datos no solamente mejora las operaciones diarias de las orga- 
+-- nizaciones, sino también la calidad de las decisiones que afectan nuestras vidas. Las bases de 
+-- datos contienen un ﬂujo de datos acerca de muchos aspectos de nuestras vidas: preferencias de 
+-- consumo, uso de telecomunicaciones, historial crediticio, hábitos al ver la televisión, etc. La 
+-- tecnología de base de datos ayuda a resumir este volumen de datos en información útil para la 
+-- toma de decisiones. Los directivos utilizan la información obtenida para la toma de decisiones 
+-- a largo plazo, como invertir en plantas y equipo, ubicar tiendas, agregar elementos nuevos al 
+-- inventario e iniciar nuevos negocios
+ 
+
+ 🧨 <Paginas 29>
+
+
+
+
+ ---------------------------------------------------------------------------------------------------------------
+
+ 🧨 <Paginas 0> 
