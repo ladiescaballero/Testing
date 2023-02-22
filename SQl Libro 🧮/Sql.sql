e32af7de25104b73b1e772beb80fb964584ed87a
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-02-22 12:28:31 -0300

    12:28

--- SQl Libro 🧮/Sql.sql
+++ SQl Libro 🧮/Sql.sql
@@ -1,43 +1,13 @@
- 🧨 <Paginas 20>
-    -- Contenido breve
-    -- Glosario
-
- 🧨 <Paginas 21>
-    -- Glosario
-
- 🧨 <Paginas 22>
-    -- Glosario
-
- 🧨 <Paginas 23> 
-    -- Glosario
-
- 🧨 <Paginas 24> 
-    -- Glosario
-
- 🧨 <Paginas 25> 
-    -- Glosario
-
- 🧨 <Paginas 26>
-
- 🧨 <Paginas 27>
-    -- Introducción 
-    -- a los ambientes 
-    -- de base de datos
-
- 🧨 <Paginas 27>
-    -- Vacio
-
- 🧨 <Paginas 28>
-    -- Introducción a la 
-    -- administración de base 
-    -- de datos
-
- 🧨 <Paginas 29>
-    -- Panorama general
-    
- 🧨 <Paginas 30>
-    -- 1.1 Características de la base de datos
+ 🧨 <Paginas 32 > 
+    -- FIGURA 1.3 
+    -- Ilustración de la base 
+    -- de datos simplificada 
+    -- de un hospital    
     ....
+    -- 1.2 Características de los sistemas de administración 
+    -- de bases de datos  
+    ...
+    -- 1.2.1 Definición de base de datos    
 
  🧨 <Paginas 31>    
     -- FIGURA 1.1
@@ -52,282 +22,54 @@
        ........
        ........ 
 
- 🧨 <Paginas 32 > 
-    -- FIGURA 1.3 
-    -- Ilustración de la base 
-    -- de datos simplificada 
-    -- de un hospital    
+ 🧨 <Paginas 30>
+    -- 1.1 Características de la base de datos
     ....
-    -- 1.2 Características de los sistemas de administración 
-    -- de bases de datos  
-    ...
-    -- 1.2.1 Definición de base de datos      
 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
- 🧨 <Paginas > 
+ 🧨 <Paginas 29>
+    -- Panorama general
+
+ 🧨 <Paginas 28>
+    -- Introducción a la 
+    -- administración de base 
+    -- de datos
+ 🧨 <Paginas 27>
+    -- Vacio
+
+ 🧨 <Paginas 26>
+    -- Introducción 
+    -- a los ambientes 
+    -- de base de datos
+
+ 🧨 <Paginas 25> 
+    -- Glosario
+
+ 🧨 <Paginas 24> 
+    -- Glosario
+
+ 🧨 <Paginas 23> 
+    -- Glosario
+
+ 🧨 <Paginas 22>
+    -- Glosario
+
+ 🧨 <Paginas 21>
+    -- Glosario
+
+ 🧨 <Paginas 20>
+    -- Contenido breve
+    -- Glosario
 
 
 
- ---------------------------------------------------------------------------------------------------
- 🧨 <Paginas > 
+
+
+
+
+
+
+
+
+
+
+
