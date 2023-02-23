5d0df9f06d029c8dd20c8d86f25c85e1e51e4a72
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-02-23 10:21:57 -0300

    10:21

--- SQl Libro 🧮/Sql.sql
+++ SQl Libro 🧮/Sql.sql
@@ -1,187 +1,193 @@
-🧨 <Paginas 43 > 
+🧨  <Paginas 43 > 
 
-
+	--  FIGURA 1.13 
+	--  Arreglos típicos de 
+	--  cliente-servidor entre 
+	--  una base de datos y el 
+	--  software  
+	 
+    -- 1.5 Impactos organizacionales de la tecnología de base de datos
 
 
 
 
 🧨 <Paginas 42 > 
 
-   --  TABLA 1.4 
-   --  Ejemplo de la base 
-   --  de datos de la universidad que ilustra las 
-   --  diferencias entre los 
-   --  niveles de esquema  
+	--  TABLA 1.4 
+	--  Ejemplo de la base 
+	--  de datos de la universidad que ilustra las 
+	--  diferencias entre los 
+	--  niveles de esquema  
 
-   --  1.4.2 Procesamiento distribuido y la arquitectura 
-   --  cliente-servidor
+	--  1.4.2 Procesamiento distribuido y la arquitectura 
+	--  cliente-servidor
 
-   --  arquitectura 
-   --  cliente-servidor
-   --  un arreglo de componentes (clientes y 
-   --  servidores) y datos 
-   --  entre computadoras 
-   --  conectadas a una red. 
-   --  La arquitectura clienteservidor soporta el procesamiento eficiente de 
-   --  mensajes (solicitudes de 
-   --  servicio) entre los clientes y los servidores.
+	--  arquitectura 
+	--  cliente-servidor
+	--  un arreglo de componentes (clientes y 
+	--  servidores) y datos 
+	--  entre computadoras 
+	--  conectadas a una red. 
+	--  La arquitectura clienteservidor soporta el procesamiento eficiente de 
+	--  mensajes (solicitudes de 
+	--  servicio) entre los clientes y los servidores.
 
 🧨 <Paginas 41 > 
 
-   --  FIGURA 1.12 
-   --  Arquitectura de los 
-   --  tres esquemas  
+	--  FIGURA 1.12 
+	--  Arquitectura de los 
+	--  tres esquemas  
 
-   --  independencia 
-   --  de datos
+	--  independencia 
+	--  de datos
 
-   --  arquitectura de los 
-   --  tres esquemas  
+	--  arquitectura de los 
+	--  tres esquemas  
 
 🧨 <Paginas 40 > 
 
-   -- 1.4 Arquitecturas de los sistemas de administración 
-   -- de bases de datos
+	-- 1.4 Arquitecturas de los sistemas de administración 
+	-- de bases de datos
 
-   -- 1.4.1 Independencia de datos y la arquitectura 
-   -- de los tres esquemas
+	-- 1.4.1 Independencia de datos y la arquitectura 
+	-- de los tres esquemas
 
 🧨 <Paginas 39 > 
  
-   1.3.2 El mercado actual del software de base de datos
+	1.3.2 El mercado actual del software de base de datos
 
  🧨 <Paginas 38 >
 
-   -- TABLA 1.2
-   -- Breve evolución de la 
-   -- tecnología de base de 
-   -- datos
+	-- TABLA 1.2
+	-- Breve evolución de la 
+	-- tecnología de base de 
+	-- datos
 
-   -- 1.3.1 Evolución de la tecnología de bases de datos
+	-- 1.3.1 Evolución de la tecnología de bases de datos
 
  🧨 <Paginas 37 >
 
-   -- 1.2.5 Funciones de terceros
-   -- Desarrollo de la tecnología de base de datos 
-   -- y la estructura de mercado
+	-- 1.2.5 Funciones de terceros
+	-- Desarrollo de la tecnología de base de datos 
+	-- y la estructura de mercado
 
  🧨 <Paginas 36 >
 
-   --   FIGURA 1.9
-   -- Forma de Microsoft 
-   -- Access para asignar 
-   -- cursos a un profesor 
-   
-   -- FIGURA 1.10 
-   -- Reporte de Microsoft 
-   -- Access de la carga de 
-   -- trabajo de un profesor
+	--   FIGURA 1.9
+	-- Forma de Microsoft 
+	-- Access para asignar 
+	-- cursos a un profesor 
+	
+	-- FIGURA 1.10 
+	-- Reporte de Microsoft 
+	-- Access de la carga de 
+	-- trabajo de un profesor
 
-   -- 1.2.4 Funciones de soporte para las operaciones 
-   -- de base de datos
+	-- 1.2.4 Funciones de soporte para las operaciones 
+	-- de base de datos
  
  🧨 <Paginas 35 >
-   --   FIGURA 1.7 
-   -- Ventana de diseño de 
-   -- consultas de Microsoft Access   
+	--   FIGURA 1.7 
+	-- Ventana de diseño de 
+	-- consultas de Microsoft Access   
 
-   -- FIGURA 1.8 
-   -- Resultado de ejecutar 
-   -- la consulta de la 
-   -- figura 1.7
+	-- FIGURA 1.8 
+	-- Resultado de ejecutar 
+	-- la consulta de la 
+	-- figura 1.7
 
-   -- 1.2.3 Desarrollo de aplicaciones e interfase 
-   -- del lenguaje procedural
+	-- 1.2.3 Desarrollo de aplicaciones e interfase 
+	-- del lenguaje procedural
 
  🧨 <Paginas 34 >
  
-    -- FIGURA 1.6 
-    -- Ventana de definición 
-    -- Entidad Relación de 
-    -- Microsoft Access
+	 -- FIGURA 1.6 
+	 -- Ventana de definición 
+	 -- Entidad Relación de 
+	 -- Microsoft Access
 
-    -- 1.2.2 Acceso no procedural
+	 -- 1.2.2 Acceso no procedural
  
  🧨 <Paginas 33 >
 
-    -- TABLA 1.1 
-    -- Resumen de las funciones generales de 
-    -- los DBMS
+	 -- TABLA 1.1 
+	 -- Resumen de las funciones generales de 
+	 -- los DBMS
 
-    -- FIGURA 1.4 Visualización de la Tabla Estudiante desde Microsoft Access
+	 -- FIGURA 1.4 Visualización de la Tabla Estudiante desde Microsoft Access
 
-    -- FIGURA 1.5
-    -- Ventana Definición 
-    -- de Tabla en Microsoft 
-    -- Access
+	 -- FIGURA 1.5
+	 -- Ventana Definición 
+	 -- de Tabla en Microsoft 
+	 -- Access
 
-    -- SQL 
-    -- lenguaje estándar de 
-    -- base de datos que incluye sentencias para la 
-    -- definición, la manipulación y el control de la 
-    -- base de datos
+	 -- SQL 
+	 -- lenguaje estándar de 
+	 -- base de datos que incluye sentencias para la 
+	 -- definición, la manipulación y el control de la 
+	 -- base de datos
  
  🧨 <Paginas 32 > 
-    -- FIGURA 1.3 
-    -- Ilustración de la base 
-    -- de datos simplificada 
-    -- de un hospital    
-    ....
-    -- 1.2 Características de los sistemas de administración 
-    -- de bases de datos  
-    ...
-    -- 1.2.1 Definición de base de datos    
+	 -- FIGURA 1.3 
+	 -- Ilustración de la base 
+	 -- de datos simplificada 
+	 -- de un hospital    
+	 ....
+	 -- 1.2 Características de los sistemas de administración 
+	 -- de bases de datos  
+	 ...
+	 -- 1.2.1 Definición de base de datos    
 
  🧨 <Paginas 31>    
-    -- FIGURA 1.1
-    -- Ilustración simplificada de la base de 
-    -- datos universitaria
-    ....
-    -- FIGURA 1.2 
-    -- Ilustración de la base 
-    -- de datos simplificada 
-    -- del departamento de 
-    -- distribución de agua
-       ........
-       ........ 
+	 -- FIGURA 1.1
+	 -- Ilustración simplificada de la base de 
+	 -- datos universitaria
+	 ....
+	 -- FIGURA 1.2 
+	 -- Ilustración de la base 
+	 -- de datos simplificada 
+	 -- del departamento de 
+	 -- distribución de agua
+		 ........
+		 ........ 
 
  🧨 <Paginas 30>
-    -- 1.1 Características de la base de datos
-    ....
+	 -- 1.1 Características de la base de datos
+	 ....
 
  🧨 <Paginas 29>
-    -- Panorama general
+	 -- Panorama general
 
  🧨 <Paginas 28>
-    -- Introducción a la 
-    -- administración de base 
-    -- de datos
+	 -- Introducción a la 
+	 -- administración de base 
+	 -- de datos
  🧨 <Paginas 27>
-    -- Vacio
+	 -- Vacio
 
  🧨 <Paginas 26>
-    -- Introducción 
-    -- a los ambientes 
-    -- de base de datos
+	 -- Introducción 
+	 -- a los ambientes 
+	 -- de base de datos
 
  🧨 <Paginas 25> 
-    -- Glosario
+	 -- Glosario
 
  🧨 <Paginas 24> 
-    -- Glosario
+	 -- Glosario
 
  🧨 <Paginas 23> 
-    -- Glosario
+	 -- Glosario
 
  🧨 <Paginas 22>
-    -- Glosario
+	 -- Glosario
 
  🧨 <Paginas 21>
-    -- Glosario
+	 -- Glosario
 
  🧨 <Paginas 20>
-    -- Contenido breve
-    -- Glosario
+	 -- Contenido breve
+	 -- Glosario
 
 
 
