3226f950293758ab0dd16f74fb4f02ce7931229d
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-02-27 10:59:13 -0300

    10:59

--- SQl Libro 🧮/SqlLibro_Consultas.sql
+++ SQl Libro 🧮/SqlLibro_Consultas.sql
@@ -1,5 +1,31 @@
-🧵 <Paginas 2 >
+🧵 <Paginas 16 >
 
+Capítulo 6: Gestión de seguridad en la base de datos
+PARTE II Acceso y modificación de datos
+Capítulo 7: Consulta de datos de SQL
+Capítulo 8: Modificar datos SQL
+Capítulo 9: Utilizar predicados
+Capítulo 10: Trabajar con funciones y expresiones de valor
+Capítulo 11: Acceder a múltiples tablas
+Capítulo 12: Utilizar subconsultas para acceder y modificar datos
+PARTE III Acceso avanzado a los datos
+
+
+
+
+
+🧵 <Paginas 15 >
+
+    -- PARTE I: Bases de datos relacionales y SQL 
+    -- Capítulo 1: Introducción a las bases de datos relacionales y a SQL
+    -- Capítulo 2: Trabajo con el entorno SQL
+    -- Capítulo 3: Creación y modificación de tablas
+    -- Capítulo 4: Implementación de la integridad de datos 
+    -- Capítulo 5: Creación de vistas en SQL
+
+🧵 <Paginas 7 >
+
+    -- Contenido
 
 
 🧵 <Paginas 1 >
