8bcca4c1274392d81fcf21716929af4c71dc03fe
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-02-27 11:31:33 -0300

    11:31

--- SQl Libro 🧮/SqlLibro_Consultas.sql
+++ SQl Libro 🧮/SqlLibro_Consultas.sql
@@ -1,17 +1,27 @@
+🧵 <Paginas 17 >
+
+    -- Capítulo 13: Crear rutinas invocadas por SQL
+    -- Capítulo 14: Crear activadores SQL
+    -- Capítulo 15: Utilizar cursores SQL
+    -- Capítulo 16: Manejar transacciones SQL 
+    -- Capítulo 17: Acceder a datos SQL desde un programa host 
+    -- Capítulo 18: Trabajar con datos XML
+    -- PARTE IV Apéndices
+    -- Apéndice A: Respuestas a los autoexámenes
+    -- Apéndice B: Palabras clave de SQL:2006
+    -- Apéndice C: Código SQL utilizado en los ejercicios Pruebe esto
+
 🧵 <Paginas 16 >
 
-Capítulo 6: Gestión de seguridad en la base de datos
-PARTE II Acceso y modificación de datos
-Capítulo 7: Consulta de datos de SQL
-Capítulo 8: Modificar datos SQL
-Capítulo 9: Utilizar predicados
-Capítulo 10: Trabajar con funciones y expresiones de valor
-Capítulo 11: Acceder a múltiples tablas
-Capítulo 12: Utilizar subconsultas para acceder y modificar datos
-PARTE III Acceso avanzado a los datos
-
-
-
+-- Capítulo 6: Gestión de seguridad en la base de datos
+-- PARTE II Acceso y modificación de datos
+-- Capítulo 7: Consulta de datos de SQL
+-- Capítulo 8: Modificar datos SQL
+-- Capítulo 9: Utilizar predicados
+-- Capítulo 10: Trabajar con funciones y expresiones de valor
+-- Capítulo 11: Acceder a múltiples tablas
+-- Capítulo 12: Utilizar subconsultas para acceder y modificar datos
+-- PARTE III Acceso avanzado a los datos
 
 
 🧵 <Paginas 15 >
@@ -26,7 +36,6 @@ PARTE III Acceso avanzado a los datos
 🧵 <Paginas 7 >
 
     -- Contenido
-
 
 🧵 <Paginas 1 >
 fundamentos_de_sql_3edi_oppel.pdf
