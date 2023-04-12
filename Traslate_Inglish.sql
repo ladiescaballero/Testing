d3d11b40392b3f0b814842d05a9aa625ec32b3e7
ladiescaballero <122918285+ladiescaballero@users.noreply.github.com>
2023-04-12 01:30:28 -0400

    1:30

---
+++ Traslate_Inglish.sql
@@ -0,0 +1,5 @@
+🅰Sin embargo, un entorno OLTP no es adecuado para la elaboración de informes porque un modelo normalizado
+suele incluir muchas tablas (una por cada entidad)🅱 
+
+Con relaciones complejas. Incluso los informes más sencillos requieren la unión de muchas tablas, lo que da lugar a consultas complejas y de bajo rendimiento.
+Puede implementar una base de datos OLTP en SQL Server y gestionarla y consultarla con T-SQL.
